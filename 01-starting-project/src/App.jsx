c861455eda8df13cae254392fc0eddd610e295e4
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2024-08-10 10:16:25 -0500

    Sperated Components

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,35 +1,7 @@
-import ReactImg from './assets/react-core-concepts.png'
-import ComponentImg from './assets/components.png'
 import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcepts.jsx';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header(){
-  return (
-    <header>
-        <img src={ReactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  )
-}
-
-function CoreConcept(props){
-  return (
-    <li>
-      <img src = {props.image}></img>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  )
-}
 function App() {
   return (
     <div>
