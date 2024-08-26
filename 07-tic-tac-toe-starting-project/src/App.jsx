1ab23adcfc42cbc9d2b4e4907e60d9be2cdabaf5
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2024-08-26 16:36:31 -0500

    Added GameOver screen

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,6 +3,8 @@ import GameBoard from "./Components/GameBoard"
 import Player from "./Components/Player";
 import Log from './Components/Log';
 import { WINNING_COMBINATIONS } from "./winning_combos";
+import GameOver from "./Components/GameOver";
+
 
 const initialGB = 
 [[null,null,null],
@@ -25,25 +27,26 @@ function App() {
   // const[activePlayer,setActivePlayer] = useState('X');
   let winner = null;
   const activePlayer = deriveActivePlayer(gameTurns);
+  let draw = (gameTurns.length === 9 && !winner);
 
   let gameBoard = initialGB;
 
-    for(const turn of gameTurns){
-        const {square,player} = turn;
-        gameBoard[square.row][square.col] = player;
-    }
+  for(const turn of gameTurns){
+      const {square,player} = turn;
+      gameBoard[square.row][square.col] = player;
+  }
 
-    for (const combo of WINNING_COMBINATIONS){
-      let first = gameBoard[combo[0].row][combo[0].column]
-      let second = gameBoard[combo[1].row][combo[1].column]
-      let third = gameBoard[combo[2].row][combo[2].column]
+  for (const combo of WINNING_COMBINATIONS){
+    let first = gameBoard[combo[0].row][combo[0].column]
+    let second = gameBoard[combo[1].row][combo[1].column]
+    let third = gameBoard[combo[2].row][combo[2].column]
 
-      if (first === second && second === third && first !== null){
-        
-        winner = first;
-        console.log(winner+" won!");
-      }
+    if (first === second && second === third && first !== null){
+      
+      winner = first;
+      console.log(winner+" won!");
     }
+  }
 
 
   function selectSquare(rowIdx,colIdx){
@@ -65,7 +68,7 @@ function App() {
           <Player name = "Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player name = "Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        {winner && <p>{winner} You Won!!</p>}
+        {(winner || draw) && <GameOver winner={winner}/>}
         <GameBoard onSelectSquare={selectSquare} gameBoard = {gameBoard} />
       </div>
       <Log turns={gameTurns}/> 
